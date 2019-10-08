22bf256136998fdbad61116e2c017381b340ca88
sst52 <saitejplacements@gmail.com>
2019-10-08 10:30:44 +0530

    Learning Creation of apps and about models.py

--- DBLearn/models.py
+++ DBLearn/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product():
+	title = models.TextField()
+	description = models.TextField()
+	price = models.TextFiled()
+	
--- ELearn/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElearnConfig(AppConfig):
-    name = 'ELearn'
