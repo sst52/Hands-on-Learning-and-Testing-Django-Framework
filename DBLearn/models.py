96d5153f08efdcd6c5f2f9952a6eef0bbf516733
sst52 <saitejplacements@gmail.com>
2019-10-08 23:14:43 +0530

    Migrated the app

--- DBLearn/models.py
+++ DBLearn/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Product(models.Model):
 	title = models.TextField()
 	description = models.TextField()
-	price = models.TextFiled()
+	price = models.TextField()
+	summary = models.TextField(default='this is cool')
 	
