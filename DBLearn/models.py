7f02bf53b3526360162fec644d99dab62c92dcc1
sst52 <saitejplacements@gmail.com>
2019-10-09 23:31:16 +0530

    Testing Decimal Places

--- DBLearn/models.py
+++ DBLearn/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Product(models.Model):
 	title = models.CharField(max_length=4) # Now we cannot enter more than 4 characters inside the character field....
 	description = models.TextField()
-	price = models.TextField()
+	price = models.DecimalField(decimal_places=2,max_digits=10000)
 	summary = models.TextField(default='this is cool')
 	
