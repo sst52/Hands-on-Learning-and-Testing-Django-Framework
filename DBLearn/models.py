af11e77005f01ce2576e26da3a07de0dd3b126f8
sst52 <saitejplacements@gmail.com>
2019-10-09 23:03:17 +0530

    Created a character field of length 4 and tested it

--- DBLearn/models.py
+++ DBLearn/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-	title = models.TextField()
+	title = models.CharField(max_length=4) # Now we cannot enter more than 4 characters inside the character field....
 	description = models.TextField()
 	price = models.TextField()
 	summary = models.TextField(default='this is cool')
